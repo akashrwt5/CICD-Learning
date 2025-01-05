37829f1345c3f5aeb8616f487b9ec5fa4ee4b6aa
Akash Rawat <akashrwt5@gmail.com>
2025-01-05 17:17:53 +0530

    Initial Commit

---
+++ CICD Learning/CICD_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_LearningApp.swift
+//  CICD Learning
+//
+//  Created by Akash Rawat on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CICD_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICD LearningTests/CICD_LearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_LearningTests.swift
+//  CICD LearningTests
+//
+//  Created by Akash Rawat on 05/01/25.
+//
+
+import Testing
+@testable import CICD_Learning
+
+struct CICD_LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
